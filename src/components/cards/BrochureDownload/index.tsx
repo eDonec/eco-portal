105bba785751113cfa88c134f64f47218f990199
nizar-zouaoui <drekieki244@gmail.com>
2025-08-29 15:58:00 +0100

    fix: fixing download link for flyer

--- src/components/cards/BrochureDownload/index.tsx
+++ src/components/cards/BrochureDownload/index.tsx
@@ -3,7 +3,8 @@ const BrochureDownloadCard = () => {
     <>
       <p>
         <a
-          href="/public/flyer_equipe_economique_france_en_tunisie_-_2025___maj-11_04.pdf"
+          href="/flyer_equipe_economique_france_en_tunisie_-_2025___maj-11_04.pdf"
+          download
           className="inline-block rounded-md border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 px-3 py-2 text-sm text-blue-700 dark:text-blue-300 hover:bg-gray-50 dark:hover:bg-gray-800 transition-colors shadow-sm"
         >
           Télécharger la brochure (PDF)
